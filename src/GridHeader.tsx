aaa95c230b4e31a55f53455102d63b17a1c9dfce
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-04 11:28:26 +0200

    created basic header (nonresponsive)

--- src/GridHeader.tsx
+++ src/GridHeader.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './GridHeaderStyle.sass'
 
 function GridHeader() {
     return(
-    <div>
-        <p>Header Scaffold</p>
+    <div className="Container">
+        <div className="Element">
+            <div className="ElementHeader">
+                <p>Utilizator</p>
+            </div>
+            <div className="ElementContent">
+                <p>Nume</p>
+                <p>Prenume</p>
+            </div>
+        </div>
+        <div className="Element">
+        <div className="ElementHeader">
+                <p>Detalii</p>
+            </div>
+            <div className="ElementContent">
+                <p>Email</p>
+                <p>Numar de Telefon</p>
+            </div>
+        </div>
     </div>)
 }
 
