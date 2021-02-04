cfb83875c5d015f057fc93f8a6d485487087165a
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-04 23:41:35 +0200

    Implemented parent element for header (nested parts)

--- src/GridHeader.tsx
+++ src/GridHeader.tsx
@@ -1,41 +1,19 @@
 import React from 'react';
+import GridHeaderColumns from './GridHeaderColumns'
 import './GridHeaderStyle.sass'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilter } from '@fortawesome/free-solid-svg-icons'
 
 function GridHeader() {
     return(
-    <div className="Container">
-        <div className="Element">
-            <div className="ElementHeader">
+    <div>
+        <div className="Container">
+            <div className="HeaderName">
                 <p>Utilizator</p>
             </div>
-            <div className="ElementContent">
-                <div className="ElementCell">
-                    <p>Nume</p>
-                    <FontAwesomeIcon className="Icon" icon={faFilter} />
-                </div>
-                <div className="ElementCell">
-                    <p>Prenume</p>
-                    <FontAwesomeIcon className="Icon" icon={faFilter} />
-                </div>
-            </div>
-        </div>
-        <div className="Element">
-        <div className="ElementHeader">
+            <div className="HeaderName">
                 <p>Detalii</p>
             </div>
-            <div className="ElementContent">
-                <div className="ElementCell">
-                <p>Email</p>
-                <FontAwesomeIcon className="Icon" icon={faFilter} />
-                </div>
-                <div className="ElementCell">
-                <p>Numar de Telefon</p>
-                <FontAwesomeIcon className="Icon" icon={faFilter} />
-                </div>
-            </div>
         </div>
+        <GridHeaderColumns />
     </div>)
 }
 
