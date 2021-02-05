d21e33aed303da498c74169287281f4109360933
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-05 12:02:41 +0200

    Added separate class for each column

--- src/GridHeaderColumns.tsx
+++ src/GridHeaderColumns.tsx
@@ -6,19 +6,19 @@ import { faFilter } from '@fortawesome/free-solid-svg-icons'
 function GridHeaderColumns() {
     return(
     <div className="ColumnContainer">
-        <div className="Column">
+        <div className="Column First">
             <p>Prenume</p>
             <FontAwesomeIcon className="Icon" icon={faFilter}/>
         </div>
-        <div className="Column">
+        <div className="Column Second">
             <p>Nume</p>
             <FontAwesomeIcon className="Icon" icon={faFilter}/>
         </div>
-        <div className="Column">
+        <div className="Column Third">
             <p>Email</p>
             <FontAwesomeIcon className="Icon" icon={faFilter}/>
         </div>
-        <div className="Column">
+        <div className="Column Fourth">
             <p>Nr Telefon</p>
             <FontAwesomeIcon className="Icon" icon={faFilter}/>
         </div>
