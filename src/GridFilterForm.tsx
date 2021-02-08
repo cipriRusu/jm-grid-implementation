5d8fabf9b0456292706e96250c261b7a87b48072
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-08 19:50:04 +0200

    Implemented single form toggling per click

--- src/GridFilterForm.tsx
+++ src/GridFilterForm.tsx
@@ -4,9 +4,9 @@ import './GridFilterFormStyle.scss';
 
 function GridFilterForm(props: any) {
     return(
-        <Form style={{ display: props.componentVisible }}>
+        <Form style={props.clickedItem === props.countItem ? { display: props.componentVisible } : {}}>
             <Form.Group>
-                <br></br>
+                <br/>
                 <Form.Row>
                     <Form.Check />
                     <Form.Label className="formLabel">Indentic cu: </Form.Label>
--- src/GridHeaderColumns.tsx
+++ src/GridHeaderColumns.tsx
@@ -6,8 +6,10 @@ import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
 function GridHeaderColumns() {
     let[visible, toggleVisibility] = useState('none')
+    let[clicked, setClicked] = useState(0)
 
-    function toggleHandler() {
+    function toggleHandler(elem: any) {
+        setClicked(elem)
         return (visible === "none" ? 
                     toggleVisibility('block') : 
                 visible === 'block' ? 
@@ -20,33 +22,33 @@ function GridHeaderColumns() {
         <div className="ColumnContainer">
             <div className="Column First">
                 <p>Prenume</p>
-                <FontAwesomeIcon onClick={toggleHandler} className="Icon" icon={faFilter}/>
+                <FontAwesomeIcon onClick={() => toggleHandler(1)} className="Icon" icon={faFilter}/>
             </div>
             <div className="Column Second">
                 <p>Nume</p>
-                <FontAwesomeIcon onClick={toggleHandler} className="Icon" icon={faFilter}/>
+                <FontAwesomeIcon onClick={() => toggleHandler(2)} className="Icon" icon={faFilter}/>
             </div>
             <div className="Column Third">
                 <p>Email</p>
-                <FontAwesomeIcon onClick={toggleHandler} className="Icon" icon={faFilter}/>
+                <FontAwesomeIcon onClick={() => toggleHandler(3)} className="Icon" icon={faFilter}/>
             </div>
             <div className="Column Fourth">
                 <p>Nr Telefon</p>
-                <FontAwesomeIcon onClick={toggleHandler} className="Icon" icon={faFilter}/>
+                <FontAwesomeIcon onClick={() => toggleHandler(4)} className="Icon" icon={faFilter}/>
             </div>
         </div>
         <div style={{ display: "flex"}}>
             <div className="Column">
-                <GridFilterForm componentVisible={visible}/>
+                <GridFilterForm countItem={1} clickedItem={clicked} componentVisible={visible}/>
             </div>
             <div className="Column">
-                <GridFilterForm componentVisible={visible}/>
+                <GridFilterForm countItem={2} clickedItem={clicked} componentVisible={visible}/>
             </div>
             <div className="Column">
-                <GridFilterForm componentVisible={visible}/>
+                <GridFilterForm countItem={3} clickedItem={clicked} componentVisible={visible}/>
             </div>
             <div className="Column">
-                <GridFilterForm componentVisible={visible}/>
+                <GridFilterForm countItem={4} clickedItem={clicked} componentVisible={visible}/>
             </div>
         </div>
     </div>)
