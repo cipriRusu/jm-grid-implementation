8f735bafd6b04787f21847c5ecf54e4ecb999d68
unknown <vlad.valentina.diana@gmail.com>
2021-02-11 15:32:01 +0200

    Resolved carret direction

--- src/GridTools/SavedFilters/SavedFilters.tsx
+++ src/GridTools/SavedFilters/SavedFilters.tsx
@@ -1,38 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './SavedFilters.scss';
 
 
-class SavedFilters extends Component<{}, {}>{
-    state = {
-        isDropdownShown: false
-    }
+const SavedFilters = () =>{
+    return (
+        <Dropdown id="saved-filters">
+            <Dropdown.Toggle variant="secondary" className="dropdown-basic grid-button">
+            <i className="icon-star icon-large"></i>
+            <span>Saved Filters</span>
+            </Dropdown.Toggle>
 
-    handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>): void => {
-        this.setState({isDropdownShown: !this.state.isDropdownShown});
-    }
-    handleOutsideClick = (): void => {
-        this.setState({isDropdownShown: false});
-    }
-    render(){
-        return (
-            <Dropdown id="saved-filters" onClick={this.handleClick.bind(this)}>
-                <Dropdown.Toggle variant="secondary" className="dropdown-basic grid-button" onBlur={this.handleOutsideClick}>
-                <i className="icon-star icon-large"></i>
-                <span>Saved Filters</span>
-                <i className={this.state.isDropdownShown ? "icon-caret-up": "icon-caret-down"}></i>
-                </Dropdown.Toggle>
-    
-                <Dropdown.Menu className="custom-dropdown">
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-           
-        );
-    }
-   
+            <Dropdown.Menu className="custom-dropdown">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+        
+    );
 }
 
 export default SavedFilters;
--- src/GridTools/ViewPart/ViewPart.tsx
+++ src/GridTools/ViewPart/ViewPart.tsx
@@ -1,39 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './ViewPart.scss';
 
-type ViewPartState = {
-    isDropdownShown: boolean
-}
-class ViewPart extends Component<{}, ViewPartState>{
-    state = {
-        isDropdownShown: false
-    }
-
-    handleClick = (): void => {
-        this.setState({isDropdownShown: !this.state.isDropdownShown});
-    }
-    handleOutsideClick = (): void => {
-        this.setState({isDropdownShown: false});
-    }
-    render(){
-        return (
-            <Dropdown id="view-part" onClick={this.handleClick} >
-                <Dropdown.Toggle variant="secondary" className="dropdown-basic grid-button" onBlur={this.handleOutsideClick}>
-                    <i className="icon-eye-open icon-large"></i>
-                    <span>View Part</span>
-                    <i className={this.state.isDropdownShown ? "icon-caret-up": "icon-caret-down"}></i>
-                </Dropdown.Toggle>
-        
-                <Dropdown.Menu className="custom-dropdown">
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-                
-            );
-    }
+const ViewPart = () => {
+    return (
+        <Dropdown id="view-part">
+            <Dropdown.Toggle variant="secondary" className="dropdown-basic grid-button">
+                <i className="icon-eye-open icon-large"></i>
+                <span>View Part</span>
+            </Dropdown.Toggle>
+    
+            <Dropdown.Menu className="custom-dropdown">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+            
+        );
+    
 
 }
 
