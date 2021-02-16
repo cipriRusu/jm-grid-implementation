7e871e4e2c6c379eea645cf56b24c9c9d41952e1
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-16 18:48:51 +0200

    Implemented column header component as Class

--- src/Grid/GridBody/GridHeader/ColumnHeader.tsx
+++ src/Grid/GridBody/GridHeader/ColumnHeader.tsx
@@ -2,34 +2,40 @@ import React from 'react';
 import './ColumnHeader.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { iColumnHeaderParameters } from './iColumnHeaderParameters';
 
 const CustomToggle = React.forwardRef(( props: any , ref: any ) => (
-    <a
-      href=""
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        props.onClick(e);
-      }}
-    >
-      {props.children}
-    </a>
-  ));
-  
-function ColumnHeader(props: any) {
-    return (<div className={`column-header ${props.className}`}>    
-              <p>{props.columnName}</p>
-              <Dropdown>
-                <Dropdown.Toggle as={CustomToggle}>
-                  <i className="icon-column fa fa-filter" aria-hidden="true"></i>
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                <Dropdown.Header>Filtreaza rezultate: </Dropdown.Header>
-                  <Dropdown.Item> Exact ca: </Dropdown.Item>
-                  <Dropdown.Item> Asemanator cu: </Dropdown.Item>
-                  <Dropdown.Item> Contine: </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>) }
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      props.onClick(e);
+    }}
+  >
+    {props.children}
+  </a>
+));
 
-export default ColumnHeader
+class ColumnHeader extends React.Component<iColumnHeaderParameters> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render(){return(<div className={`column-header ${this.props.columnWidth}`}>    
+                    <p>{this.props.columnName}</p>
+                    <Dropdown>
+                      <Dropdown.Toggle as={CustomToggle}>
+                        <i className="icon-column fa fa-filter" aria-hidden="true"></i>
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu>
+                      <Dropdown.Header>Filtreaza rezultate: </Dropdown.Header>
+                      <Dropdown.Item> Exact ca: </Dropdown.Item>
+                      <Dropdown.Item> Asemanator cu: </Dropdown.Item>
+                      <Dropdown.Item> Contine: </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>)}
+}
+
+export default ColumnHeader;
