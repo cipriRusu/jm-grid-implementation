e8c37173bcf7c04f2562b95f054aa4671b0af54c
Cipri Rusu <sysuser@DESKTOP-P0HBHKN>
2021-03-18 13:39:32 +0200

    Implemented abstractions for grid cell and grid row

---
+++ grid-junior/src/Grid/Interfaces/GridBody/ICell.tsx
@@ -0,0 +1,6 @@
+import { Cell_Type } from "../../CustomTypes/Cell_Type";
+
+export interface ICell {
+    cell_type?: Cell_Type;
+    cell_content?: string;
+}
---
+++ grid-junior/src/Grid/Interfaces/GridBody/IRow.tsx
@@ -0,0 +1,5 @@
+import { ICell } from "./ICell";
+
+export interface IRow {
+    content: ICell[];
+}
