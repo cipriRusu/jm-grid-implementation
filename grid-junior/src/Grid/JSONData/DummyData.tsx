a247dd58228f38b2554bcc400743c78523e28403
Ciprian Rusu <cipri91rusu@gmail.com>
2021-03-30 11:20:00 +0300

    Implemented basic scroolbar for entries (no infinite scrool)

--- grid-junior/src/Grid/JSONData/DummyData.tsx
+++ grid-junior/src/Grid/JSONData/DummyData.tsx
@@ -18,5 +18,70 @@ export const dummy_data =
  {'Prenume': 'richard',
   'Nume': 'william',
   'Email': 'rwilliam@gmail.com',
+  'Nr Telefon': '879875675' },
+
+ {'Prenume': 'gigi',
+  'Nume': 'vasile',
+  'Email': 'gvasile@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'nelu',
+  'Nume': 'nicu',
+  'Email': 'enicu@gmail.com',
+  'Nr Telefon': '90878998' },
+  
+ {'Prenume': 'andrei',
+  'Nume': 'duncan',
+  'Email': 'adcan@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'richard',
+  'Nume': 'william',
+  'Email': 'rwilliam@gmail.com',
+  'Nr Telefon': '879875675' },
+
+  {'Prenume': 'gigi',
+  'Nume': 'vasile',
+  'Email': 'gvasile@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'nelu',
+  'Nume': 'nicu',
+  'Email': 'enicu@gmail.com',
+  'Nr Telefon': '90878998' },
+  
+ {'Prenume': 'andrei',
+  'Nume': 'duncan',
+  'Email': 'adcan@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'richard',
+  'Nume': 'william',
+  'Email': 'rwilliam@gmail.com',
+  'Nr Telefon': '879875675' },
+
+  {'Prenume': 'richard',
+  'Nume': 'william',
+  'Email': 'rwilliam@gmail.com',
+  'Nr Telefon': '879875675' },
+  
+  {'Prenume': 'gigi',
+  'Nume': 'vasile',
+  'Email': 'gvasile@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'nelu',
+  'Nume': 'nicu',
+  'Email': 'enicu@gmail.com',
+  'Nr Telefon': '90878998' },
+  
+ {'Prenume': 'andrei',
+  'Nume': 'duncan',
+  'Email': 'adcan@gmail.com',
+  'Nr Telefon': '010292991' },
+  
+ {'Prenume': 'richard',
+  'Nume': 'william',
+  'Email': 'rwilliam@gmail.com',
   'Nr Telefon': '879875675' }
 ]
