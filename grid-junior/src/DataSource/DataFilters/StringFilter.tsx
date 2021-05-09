88e240695769b4976a2bce3c01964236f818af2a
DESKTOP-SNL5584\sysuser <cipri91rusu@gmail.com>
2021-05-09 14:27:48 +0300

    bugfix: modified string filter to fit current interface

--- grid-junior/src/DataSource/DataFilters/StringFilter.tsx
+++ grid-junior/src/DataSource/DataFilters/StringFilter.tsx
@@ -7,27 +7,27 @@ export class StringFilter {
   }
 
   applyFilters(filters: IFilter[]) {
-    filters.forEach((x: any) => {
+    filters.forEach((x: IFilter) => {
       this.data = this.data.filter((y: any) => {
         switch (x.operator) {
           case 0:
-            return y[x.name].includes(x.value);
+            return y[x.name].includes(x.values);
           case 1:
-            return !y[x.name].includes(x.value);
+            return !y[x.name].includes(x.values);
           case 2:
-            return y[x.name].startsWith(x.value);
+            return y[x.name].startsWith(x.values);
           case 3:
-            return y[x.name].endsWith(x.value);
+            return y[x.name].endsWith(x.values);
           case 4:
-            return y[x.name] === x.value;
+            return y[x.name] === x.values;
           case 5:
-            return y[x.name] !== x.value;
+            return y[x.name] !== x.values;
           default:
-            return y[x.name].includes(x.value);
+            return y[x.name].includes(x.values);
         }
       });
     });
-
+    console.log(this.data);
     return this.data;
   }
 }
