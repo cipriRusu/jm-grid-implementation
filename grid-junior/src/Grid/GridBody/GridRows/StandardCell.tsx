b644238aadc31dcd967a25b53b35832605601c77
cRusu <cipri91rusu@gmail.com>
2021-05-17 13:15:37 +0300

    Lifted styled component from inside Cell (rendered outside of container)

--- grid-junior/src/Grid/GridBody/GridRows/StandardCell.tsx
+++ grid-junior/src/Grid/GridBody/GridRows/StandardCell.tsx
@@ -1,24 +1,8 @@
 import styled from "styled-components";
 import { ICell } from "../../Interfaces/GridBody/ICell";
 
-const CellStyled = styled.div`
-  @media (max-width: 30rem) {
-    .prenume-cell {
-      font-weight: 1000;
-    }
-
-    .nume-cell {
-      font-weight: 700;
-    }
-  }
-`;
-
 const StandardCell = (props: ICell) => {
-  return (
-    <CellStyled>
-      <div className={`cell ${props.standard_type}`}>{props.cell_content}</div>
-    </CellStyled>
-  );
+  return <div className={`cell ${props.cell_size}`}>{props.cell_content}</div>;
 };
 
 export default StandardCell;
