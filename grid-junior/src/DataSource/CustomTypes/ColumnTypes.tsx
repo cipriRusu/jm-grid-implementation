df67548cb76a88f48ef307a9ecb99cd54e91df1d
cRusu <cipri91rusu@gmail.com>
2021-05-19 14:15:48 +0300

    Implemneted enumerable types and abstracted ColumnSizes, ColumnTypes, and ColumnVisibility

---
+++ grid-junior/src/DataSource/CustomTypes/ColumnTypes.tsx
@@ -0,0 +1,6 @@
+export enum ColumnTypes {
+  boolean = "boolean",
+  number = "number",
+  date = "date",
+  select = "select",
+}
---
+++ grid-junior/src/DataSource/CustomTypes/ColumnVisibility.tsx
@@ -0,0 +1,5 @@
+export enum ColumnVisibility {
+  LargeVisible = "x-visible",
+  StandardVisible = "m-visbile",
+  SmallVisible = "s-visible",
+}
