5e0c6f69ac29e890b951d2f216bc3999db99901e
cRusu <cipri91rusu@gmail.com>
2021-05-24 17:59:47 +0300

    Removed CellStyled component within cell

--- grid-junior/src/Grid/GridBody/GridRows/Cell.tsx
+++ grid-junior/src/Grid/GridBody/GridRows/Cell.tsx
@@ -1,6 +1,6 @@
+import "./Cell.scss";
 import { ICell } from "../../Interfaces/GridBody/ICell";
 import StandardCell from "./StandardCell";
-import "./Cell.scss";
 import BooleanCell from "./BooleanCell";
 import SelectionCell from "./SelectionCell";
 import DateCell from "./DateCell";
@@ -12,75 +12,51 @@ const Cell = (props: { content: ICell }) => {
       case undefined:
       case "number":
         return (
-          <CellStyled
-            className={`${content.cell_visibility.join(" ")} ${
-              content.cell_collapsable
-            }`}
-          >
-            <StandardCell
-              cell_content={content.cell_content}
-              cell_key={content.cell_key}
-              cell_type={content.cell_type}
-              cell_column={content.cell_column}
-              cell_size={content.cell_size}
-              cell_visibility={content.cell_visibility}
-              cell_collapsable={content.cell_collapsable}
-            />
-          </CellStyled>
+          <StandardCell
+            cell_content={content.cell_content}
+            cell_key={content.cell_key}
+            cell_type={content.cell_type}
+            cell_column={content.cell_column}
+            cell_size={content.cell_size}
+            cell_visibility={content.cell_visibility}
+            cell_collapsable={content.cell_collapsable}
+          />
         );
       case "boolean":
         return (
-          <CellStyled
-            className={`${content.cell_visibility.join(" ")} ${
-              content.cell_collapsable
-            }`}
-          >
-            <BooleanCell
-              cell_content={content.cell_content}
-              cell_key={content.cell_key}
-              cell_type={content.cell_type}
-              cell_size={content.cell_size}
-              cell_visibility={content.cell_visibility}
-              cell_collapsable={content.cell_collapsable}
-            />
-          </CellStyled>
+          <BooleanCell
+            cell_content={content.cell_content}
+            cell_key={content.cell_key}
+            cell_type={content.cell_type}
+            cell_size={content.cell_size}
+            cell_visibility={content.cell_visibility}
+            cell_collapsable={content.cell_collapsable}
+          />
         );
       case "select":
         return (
-          <CellStyled
-            className={`${content.cell_visibility.join(" ")} ${
-              content.cell_collapsable
-            }`}
-          >
-            <SelectionCell
-              id={content.id}
-              cell_content={content.cell_content}
-              cell_key={content.cell_key}
-              cell_type={content.cell_type}
-              cell_size={content.cell_size}
-              selection_options={content.selection_options}
-              cell_visibility={content.cell_visibility}
-              cell_collapsable={content.cell_collapsable}
-            />
-          </CellStyled>
+          <SelectionCell
+            id={content.id}
+            cell_content={content.cell_content}
+            cell_key={content.cell_key}
+            cell_type={content.cell_type}
+            cell_size={content.cell_size}
+            selection_options={content.selection_options}
+            cell_visibility={content.cell_visibility}
+            cell_collapsable={content.cell_collapsable}
+          />
         );
       case "date":
         return (
-          <CellStyled
-            className={`${content.cell_visibility.join(" ")} ${
-              content.cell_collapsable
-            }`}
-          >
-            <DateCell
-              cell_content={content.cell_content}
-              cell_key={content.cell_key}
-              cell_type={content.cell_type}
-              cell_size={content.cell_size}
-              selection_options={content.selection_options}
-              cell_visibility={content.cell_visibility}
-              cell_collapsable={content.cell_collapsable}
-            />
-          </CellStyled>
+          <DateCell
+            cell_content={content.cell_content}
+            cell_key={content.cell_key}
+            cell_type={content.cell_type}
+            cell_size={content.cell_size}
+            selection_options={content.selection_options}
+            cell_visibility={content.cell_visibility}
+            cell_collapsable={content.cell_collapsable}
+          />
         );
     }
   };
