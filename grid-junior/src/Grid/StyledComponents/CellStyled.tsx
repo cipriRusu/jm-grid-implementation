f25d7a37b704579a110c40070737326887169a0a
cRusu <cipri91rusu@gmail.com>
2021-05-24 18:00:33 +0300

    Added logic for cell styling in small screen

--- grid-junior/src/Grid/StyledComponents/CellStyled.tsx
+++ grid-junior/src/Grid/StyledComponents/CellStyled.tsx
@@ -1,3 +1,20 @@
 import styled from "styled-components";
+import { Cell_Type } from "../CustomTypes/CellType";
+import { ColumnTypes } from "../CustomTypes/ColumnTypes";
+import { IColumn } from "../Interfaces/GridBody/IColumn";
+import ScreenThresholds from "./ScreenThresholds";
 
-export const CellStyled = styled.div``;
+export const CellStyled = styled.div<{
+  allColumns: IColumn[];
+  cell_type: Cell_Type;
+}>`
+  @media (max-width: ${ScreenThresholds.SmallScreen + "rem"}) {
+    grid-column: ${(props) => {
+      return props.allColumns[0].type === ColumnTypes.select &&
+        props.allColumns.length > 1 &&
+        props.cell_type === ColumnTypes.select
+        ? 1
+        : 2;
+    }};
+  }
+`;
