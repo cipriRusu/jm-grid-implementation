4b337eee2fa3de2b5680898e3c550beff742a30d
cRusu <cipri91rusu@gmail.com>
2021-06-01 03:44:23 +0300

    Created wrapper component for sidebar, button and grid components (with styled component)

--- grid-junior/src/App.tsx
+++ grid-junior/src/App.tsx
@@ -1,21 +1,35 @@
 import Grid from "./Grid/Grid";
-import React from "react";
 import { headers } from "./DataSource/GridStructure/HeadersData";
 import { DataSource } from "./DataSource/DataSource";
+import React, { useState } from "react";
+import SideBar from "./SideBar";
+import ToggleSideBar from "./ToggleSidebar";
+import { StyledApp } from "./StyledApp";
 
 const PAGE_SIZE = 11;
 const CACHE_SIZE = 33;
 
 function App() {
+  const [toggledSideBar, setToggleSideBar] = useState(false);
+
   return (
-    <div className="App">
+    <StyledApp>
+      <ToggleSideBar
+        toggledSideBar={toggledSideBar}
+        setToggleSideBar={setToggleSideBar}
+      ></ToggleSideBar>
+      <SideBar
+        toggledSideBar={toggledSideBar}
+        setToggleSideBar={setToggleSideBar}
+        headers={headers}
+      ></SideBar>
       <Grid
         data={new DataSource()}
         headers={headers}
         pageSize={PAGE_SIZE}
         cacheSize={CACHE_SIZE}
       />
-    </div>
+    </StyledApp>
   );
 }
 
