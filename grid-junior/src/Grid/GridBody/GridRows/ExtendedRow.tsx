9a55c84e8d17e7cba301294f5924ab441c312953
cRusu <cipri91rusu@gmail.com>
2021-06-01 03:49:23 +0300

    Created ExtendedRow component

--- grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
+++ grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
@@ -1,7 +1,20 @@
+import { IColumn } from "../../Interfaces/GridBody/IColumn";
 import { IRow } from "../../Interfaces/GridBody/IRow";
 
-const ExtendedRow = (props: { completeRow: IRow }) => {
-  return <p>{props.completeRow["Nume"]}</p>;
+const ExtendedRow = (props: { completeRow: IRow; allColumns: IColumn[] }) => {
+  return (
+    <>
+      <br></br>
+      {props.allColumns.map((x: IColumn, key: number) => {
+        return (
+          <div key={key} className={x.minVisibility}>
+            {x.name} : {props.completeRow[x.name].toString()}
+          </div>
+        );
+      })}
+      <br></br>
+    </>
+  );
 };
 
 export default ExtendedRow;
