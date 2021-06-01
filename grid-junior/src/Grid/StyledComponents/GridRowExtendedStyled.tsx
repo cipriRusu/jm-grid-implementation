0fb8678517c705f9a2d1972fbb9dfca7ae398669
cRusu <cipri91rusu@gmail.com>
2021-06-01 04:53:24 +0300

    Refactored media query thresholds (under minimum, minimum/medium, medium/large, above large)

--- grid-junior/src/Grid/StyledComponents/GridRowExtendedStyled.tsx
+++ grid-junior/src/Grid/StyledComponents/GridRowExtendedStyled.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import { MinimumVisibility } from "../CustomTypes/ColumnVisibility";
 import { IColumn } from "../Interfaces/GridBody/IColumn";
+import ScreenThresholds from "./ScreenThresholds";
 
 const GridRowExtendedStyled = styled.div<{
   inputColumns: IColumn[];
@@ -8,6 +10,68 @@ const GridRowExtendedStyled = styled.div<{
   grid-column: 1 / ${(props) => props.inputColumns.length};
   margin-left: 1rem;
   margin-right: 1rem;
+
+  @media (min-width: ${ScreenThresholds.LargeScreen + "rem"}) {
+    .${MinimumVisibility.MaxVisible.toString()} {
+      display: none;
+    }
+    .${MinimumVisibility.LargeVisible.toString()} {
+      display: none;
+    }
+    .${MinimumVisibility.MediumVisible.toString()} {
+      display: none;
+    }
+    .${MinimumVisibility.SmallVisible.toString()} {
+      display: none;
+    }
+  }
+
+  @media (min-width: ${ScreenThresholds.MediumScreen +
+    "rem"}) and (max-width: ${ScreenThresholds.LargeScreen + "rem"}) {
+    .${MinimumVisibility.MaxVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.LargeVisible.toString()} {
+      display: none;
+    }
+    .${MinimumVisibility.MediumVisible.toString()} {
+      display: none;
+    }
+    .${MinimumVisibility.SmallVisible.toString()} {
+      display: none;
+    }
+  }
+
+  @media (min-width: ${ScreenThresholds.SmallScreen +
+    "rem"}) and (max-width: ${ScreenThresholds.MediumScreen + "rem"}) {
+    .${MinimumVisibility.MaxVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.LargeVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.MediumVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.SmallVisible.toString()} {
+      display: none;
+    }
+  }
+
+  @media (max-width: ${ScreenThresholds.SmallScreen + "rem"}) {
+    .${MinimumVisibility.MaxVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.LargeVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.MediumVisible.toString()} {
+      display: block;
+    }
+    .${MinimumVisibility.SmallVisible.toString()} {
+      display: none;
+    }
+  }
 `;
 
 export default GridRowExtendedStyled;
