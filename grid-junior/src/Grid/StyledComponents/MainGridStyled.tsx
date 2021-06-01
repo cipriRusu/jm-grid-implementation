22e2f6fe42889539be6e7adcad261c6aaacc61f2
cRusu <cipri91rusu@gmail.com>
2021-06-01 03:50:23 +0300

    Added extra styling into MainGridStyled component

--- grid-junior/src/Grid/StyledComponents/MainGridStyled.tsx
+++ grid-junior/src/Grid/StyledComponents/MainGridStyled.tsx
@@ -17,13 +17,17 @@ const MainGrid = styled.div<{
   height: 38rem;
   overflow-y: scroll;
   background-color: gray;
+  flex: 1;
 
   @media (min-width: ${ScreenThresholds.MediumScreen +
     "rem"}) and (max-width: ${ScreenThresholds.LargeScreen + "rem"}) {
     grid-template-columns: ${(props) =>
       props.inputColumns
         .filter((x: IColumn) => {
-          return x.minVisibility !== MinimumVisibility.MaxVisible;
+          return (
+            x.minVisibility !== MinimumVisibility.MaxVisible &&
+            x.minVisibility !== MinimumVisibility.Invisible
+          );
         })
         .map((x: IColumn) => {
           return x.size + " ";
