78c23d5cc01b5c202ae85b04c752515cf1881ab7
cRusu <cipri91rusu@gmail.com>
2021-06-01 03:49:57 +0300

    Removed unused query

--- grid-junior/src/Grid/StyledComponents/GridColumnStyled.tsx
+++ grid-junior/src/Grid/StyledComponents/GridColumnStyled.tsx
@@ -4,9 +4,6 @@ import ScreenThresholds from "./ScreenThresholds";
 const GridColumn = styled.div`
   background-color: black;
 
-  @media (max-width: ${ScreenThresholds.LargeScreen + "rem"}) {
-  }
-
   @media (max-width: ${ScreenThresholds.MediumScreen + "rem"}) {
     display: none;
   }
