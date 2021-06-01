619c26054d5aa8e9a6bdda6f05536840e4c3075c
cRusu <cipri91rusu@gmail.com>
2021-06-01 04:52:42 +0300

    Wrapped main component inside styled component

--- grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
+++ grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
@@ -1,19 +1,30 @@
 import { IColumn } from "../../Interfaces/GridBody/IColumn";
 import { IRow } from "../../Interfaces/GridBody/IRow";
+import GridRowExtendedStyled from "../../StyledComponents/GridRowExtendedStyled";
 
-const ExtendedRow = (props: { completeRow: IRow; allColumns: IColumn[] }) => {
+const ExtendedRow = (props: {
+  id: string;
+  completeRow: IRow;
+  allColumns: IColumn[];
+  checkToggle: (row_key: number) => string;
+  row_key: number;
+}) => {
   return (
-    <>
-      <br></br>
+    <GridRowExtendedStyled
+      className={props.checkToggle(props.row_key)}
+      inputColumns={props.allColumns}
+    >
       {props.allColumns.map((x: IColumn, key: number) => {
         return (
           <div key={key} className={x.minVisibility}>
+            <br></br>
             {x.name} : {props.completeRow[x.name].toString()}
+            <br></br>
           </div>
         );
       })}
       <br></br>
-    </>
+    </GridRowExtendedStyled>
   );
 };
 
