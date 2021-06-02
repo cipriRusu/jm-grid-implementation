35f2ba016fd0b069808feec07a3c9f99c142a139
cRusu <cipri91rusu@gmail.com>
2021-06-02 13:32:27 +0300

    Added column remove support inside sidebar

--- grid-junior/src/SideBar.tsx
+++ grid-junior/src/SideBar.tsx
@@ -1,13 +1,42 @@
+import React from "react";
 import { IColumn } from "./Grid/Interfaces/GridBody/IColumn";
 import { IColumns } from "./Grid/Interfaces/GridBody/IColumns";
 import { IHeader } from "./Grid/Interfaces/GridBody/IHeader";
+import SideBarElement from "./SideBarElement";
 import { StyledSideBar } from "./StyledSideBar";
 
 function SideBar(props: {
   toggledSideBar: boolean;
   setToggleSideBar: (value: boolean) => void;
+  updateHeaderData: (value: [IHeader]) => void;
   headers: IHeader[];
 }) {
+  function RemoveGroup(toRemove: string) {
+    let currentHeaderData = Object.create(props.headers) as [IHeader];
+
+    currentHeaderData.forEach((x) => {
+      x.headers = x.headers.filter((y) => {
+        return y.name !== toRemove;
+      });
+    });
+
+    props.updateHeaderData(currentHeaderData);
+  }
+
+  function RemoveColumn(toRemove: string) {
+    let currentHeaderData = Object.create(props.headers) as [IHeader];
+
+    currentHeaderData.forEach((x) => {
+      x.headers.forEach((y) => {
+        y.columns = y.columns.filter((z) => {
+          return z.name !== toRemove;
+        });
+      });
+    });
+
+    props.updateHeaderData(currentHeaderData);
+  }
+
   return (
     <StyledSideBar isVisible={props.toggledSideBar}>
       <i
@@ -19,12 +48,22 @@ function SideBar(props: {
       {props.headers.map((x: IHeader) => {
         return x.headers.map((x: IColumns, key: number) => {
           return (
-            <div key={key}>
-              <p>{x.name}</p>
+            <React.Fragment key={key}>
+              <SideBarElement
+                key={key}
+                column={x}
+                removeColumn={() => RemoveGroup(x.name)}
+              ></SideBarElement>
               {x.columns.map((y: IColumn, key: number) => {
-                return <p key={key}>{y.name}</p>;
+                return (
+                  <SideBarElement
+                    key={key}
+                    column={y}
+                    removeColumn={RemoveColumn}
+                  ></SideBarElement>
+                );
               })}
-            </div>
+            </React.Fragment>
           );
         });
       })}
