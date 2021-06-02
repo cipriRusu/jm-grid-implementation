2a4520fa793be26e900427b3313da97ae3abe071
cRusu <cipri91rusu@gmail.com>
2021-06-02 18:08:42 +0300

    Created separate component for Group adding

---
+++ grid-junior/src/SideBarGroupAdd.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { StyledSideBarGroupForm } from "./StyedSideBarGroupForm";
+import { StyledSideBarElement } from "./StyledSideBarElement";
+import { StyledSideBarGroupAdd } from "./StyledSideBarGroupAdd";
+
+function SideBarGroupAdd(props: {
+  upperNewGroup: string;
+  updateUpperNewGroup: (value: string) => void;
+  addNewGroup: (value: string) => void;
+}) {
+  const [toggled, updateToggled] = useState(false);
+
+  return (
+    <StyledSideBarGroupAdd>
+      <StyledSideBarElement>
+        <div>Add New Group</div>
+        <i
+          className={`${
+            toggled === false ? "fa fa fa-plus" : "fa fa fa-minus"
+          }`}
+          aria-hidden="true"
+          onClick={() => updateToggled(toggled === true ? false : true)}
+        ></i>
+      </StyledSideBarElement>
+      <StyledSideBarGroupForm isToggled={toggled}>
+        <div>Group Name: </div>
+        <input
+          type="text"
+          value={props.upperNewGroup}
+          onInput={(e) => props.updateUpperNewGroup(e.currentTarget.value)}
+        ></input>
+        <button onClick={() => props.addNewGroup(props.upperNewGroup)}>
+          Add New Group
+        </button>
+      </StyledSideBarGroupForm>
+    </StyledSideBarGroupAdd>
+  );
+}
+
+export default SideBarGroupAdd;
