5c140627b1761df2b690f58ec33b2b48e01dda76
cRusu <cipri91rusu@gmail.com>
2021-06-02 18:11:15 +0300

    styled sidebar scroolbar

--- grid-junior/src/StyledSideBar.tsx
+++ grid-junior/src/StyledSideBar.tsx
@@ -6,6 +6,20 @@ export const StyledSideBar = styled.div<{ isVisible: boolean }>`
   width: 20%;
   padding: 0.8rem;
   display: ${(props) => (props.isVisible ? "block" : "none")};
+  overflow-y: scroll;
+  height: 38rem;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
 
   .fa {
     color: white;
