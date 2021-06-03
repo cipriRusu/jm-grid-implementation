ec36c13bcd1a5e90c21647a4703cddb1aeba28cd
cRusu <cipri91rusu@gmail.com>
2021-06-03 14:23:36 +0300

    Added branching for different value types in extended row

--- grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
+++ grid-junior/src/Grid/GridBody/GridRows/ExtendedRow.tsx
@@ -1,6 +1,11 @@
+import React from "react";
+import { DataType } from "../../CustomTypes/DataType";
 import { IColumn } from "../../Interfaces/GridBody/IColumn";
 import { IRow } from "../../Interfaces/GridBody/IRow";
 import GridRowExtendedStyled from "../../StyledComponents/GridRowExtendedStyled";
+import { BooleanRow } from "./BooleanRow";
+import { DateRow } from "./DateRow";
+import { StandardRow } from "./StandardRow";
 
 const ExtendedRow = (props: {
   id: string;
@@ -14,12 +19,44 @@ const ExtendedRow = (props: {
       className={props.checkToggle(props.row_key)}
       inputColumns={props.allColumns}
     >
+      <br></br>
       {props.allColumns.map((x: IColumn, key: number) => {
         return (
           <div key={key} className={x.minVisibility}>
-            <br></br>
-            {x.name} : {props.completeRow[x.name]}
-            <br></br>
+            {(() => {
+              switch (x.type) {
+                case "text" as DataType:
+                case "number" as DataType:
+                case "select" as DataType:
+                  return (
+                    <StandardRow
+                      name={x.name}
+                      completeRow={props.completeRow}
+                    ></StandardRow>
+                  );
+                case "boolean" as DataType:
+                  return (
+                    <BooleanRow
+                      name={x.name}
+                      completeRow={props.completeRow}
+                    ></BooleanRow>
+                  );
+                case "date" as DataType:
+                  return (
+                    <DateRow
+                      name={x.name}
+                      completeRow={props.completeRow}
+                    ></DateRow>
+                  );
+                default:
+                  return (
+                    <StandardRow
+                      name={x.name}
+                      completeRow={props.completeRow}
+                    ></StandardRow>
+                  );
+              }
+            })()}
           </div>
         );
       })}
