df4d73fd05dc8c42237b4e70a67d8a4be1b64835
cRusu <cipri91rusu@gmail.com>
2021-06-08 15:37:18 +0300

    Added dynamic header selection (possibility to choose header dynamically)

--- grid-junior/src/Grid/GridBody/GridHeader/Header.tsx
+++ grid-junior/src/Grid/GridBody/GridHeader/Header.tsx
@@ -3,36 +3,43 @@ import { useContext } from "react";
 import Column from "./Column";
 import Title from "./Title";
 import { GridContext } from "../../Grid";
-import { IColumns } from "../../Interfaces/GridBody/IColumns";
+import { IGrouping } from "../../Interfaces/GridBody/IGrouping";
 import { IColumn } from "../../Interfaces/GridBody/IColumn";
+import { IHeader } from "../../Interfaces/GridBody/IHeader";
+
+const HEADERTODISPLAY = "firstHeader";
 
 function Header() {
   const gridContext = useContext(GridContext);
 
   return (
     <div className="grid-header">
-      {gridContext.allHeaders[0].headers.map((value: IColumns, key: number) => {
-        return (
-          <div className="header-container" key={key}>
-            <Title key={key} title={value.name} columns={value.columns} />
-            <div className="column-container">
-              {value.columns.map((value: IColumn, key: number) => {
-                return (
-                  <Column
-                    key={key}
-                    name={value.name}
-                    size={value.size}
-                    type={value.type}
-                    toggled={false}
-                    collapsable={value.collapsable}
-                    minVisibility={value.minVisibility}
-                  />
-                );
-              })}
+      {gridContext.allHeaders
+        .filter((x: IHeader) => {
+          x.name === HEADERTODISPLAY;
+        })[0]
+        .headers.map((value: IGrouping, key: number) => {
+          return (
+            <div className="header-container" key={key}>
+              <Title key={key} title={value.name} columns={value.columns} />
+              <div className="column-container">
+                {value.columns.map((value: IColumn, key: number) => {
+                  return (
+                    <Column
+                      key={key}
+                      name={value.name}
+                      size={value.size}
+                      type={value.type}
+                      toggled={false}
+                      collapsable={value.collapsable}
+                      minVisibility={value.minVisibility}
+                    />
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
