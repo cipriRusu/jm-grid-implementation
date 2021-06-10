c0bc5149a08af4e42915772cc6d23ec912a78afc
cRusu <cipri91rusu@gmail.com>
2021-06-10 14:53:22 +0300

    Implemented edit button in sidebar & styling Added toggleable column component

--- grid-junior/src/SideBarElement.tsx
+++ grid-junior/src/SideBarElement.tsx
@@ -1,28 +1,61 @@
+import { useState } from "react";
 import { IColumn } from "./Grid/Interfaces/GridBody/IColumn";
 import { IGrouping } from "./Grid/Interfaces/GridBody/IGrouping";
+import SideBarColumnEdit from "./Grid/SideBarColumnEdit";
 import { StyledSideBarElement } from "./StyledSideBarElement";
 
 function SideBarElement(props: {
-  column: IColumn | IGrouping;
+  columnOrGrouping: IColumn | IGrouping;
   removeColumn: (column: string) => void;
+  editColumn: (editedColumn: IColumn, name: string) => void;
 }) {
+  const [isToggled, updateisToggled] = useState(false);
+
   return (
-    <StyledSideBarElement>
-      <p
-        style={
-          {
-            fontWeight: `${"columns" in props.column ? "bold" : ""}`,
-          } as React.CSSProperties
-        }
+    <>
+      <StyledSideBarElement>
+        <p
+          style={
+            {
+              fontWeight: `${
+                "columns" in props.columnOrGrouping ? "bold" : ""
+              }`,
+            } as React.CSSProperties
+          }
+        >
+          {props.columnOrGrouping.name}
+        </p>
+        <div className="icons-grouping">
+          {!("columns" in props.columnOrGrouping) ? (
+            <i
+              className="fa fa-pencil-square-o"
+              onClick={() => updateisToggled(isToggled === true ? false : true)}
+              aria-hidden="true"
+            ></i>
+          ) : (
+            ""
+          )}
+          <i
+            className="fa fa-minus-circle"
+            aria-hidden="true"
+            onClick={() => {
+              props.removeColumn(props.columnOrGrouping.name);
+            }}
+          ></i>
+        </div>
+      </StyledSideBarElement>
+      <div
+        style={{
+          display: `${"columns" in props.columnOrGrouping ? "none" : "block"}`,
+        }}
       >
-        {props.column.name}
-      </p>
-      <i
-        className="fa fa-minus-circle"
-        aria-hidden="true"
-        onClick={() => props.removeColumn(props.column.name)}
-      ></i>
-    </StyledSideBarElement>
+        <SideBarColumnEdit
+          isToggled={isToggled}
+          column={props.columnOrGrouping as IColumn}
+          editColumn={props.editColumn}
+        ></SideBarColumnEdit>
+      </div>
+    </>
   );
 }
 
