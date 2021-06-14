9f01087bfaeb40f5b432cf8c7c2e508aee38247b
cRusu <cipri91rusu@gmail.com>
2021-06-14 08:41:23 +0300

    Changed column sizes in miinmax intervals (for keeping sizes constant in extremely minimum cases)

--- grid-junior/src/Grid/CustomTypes/ColumnSizes.tsx
+++ grid-junior/src/Grid/CustomTypes/ColumnSizes.tsx
@@ -1,6 +1,6 @@
 export enum ColumnSizes {
-  IconColumn = "0.5fr",
-  SmallColumn = "1fr",
-  StandardColumn = "2fr",
-  LargeColumn = "3fr",
+  IconColumn = "minmax(1rem, 0.5fr)",
+  SmallColumn = "minmax(5rem, 1fr)",
+  StandardColumn = "minmax(6rem, 2fr)",
+  LargeColumn = "minmax(7rem, 3fr)",
 }
